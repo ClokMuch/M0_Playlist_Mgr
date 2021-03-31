c42563445207d66c9b583bf479b6db051e0194e8
Clok Much <tm1596113667@hotmail.com>
2021-03-31 12:23:33 +0800

    Initial

---
+++ config.py
@@ -0,0 +1,7 @@
+class config():
+    m0_prefix = "A:\\"
+    m0_floder = "_explaylist_data"
+    m0_playlist_type = ".m3u"
+    playlist_default_name = "Default"
+    support_file_type = [".m3u", ".m3u8"]
+
