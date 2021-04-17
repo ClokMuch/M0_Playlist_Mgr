df8c8f1dbf8f7cf22998f79b0934ab4b16d77961
ClokMuch <tm1596113667@gmail.com>
2021-04-17 18:23:21 +0800

    设置基本GUI（暂时完全不可用）； 改进主程序与方法库（当前版本如果无法正常使用，可暂时使用上一版本）.

--- Application.py
+++ Application.py
@@ -11,43 +11,54 @@ from random import shuffle
 
 
 # 检测设备路径，设定播放列表状态
-m0_device = methods.chk_if_device()
-if not m0_device:
-    playlist_loop_inct = False
-    double_chk_result = False
-    while not playlist_loop_inct:
+while True:
+    m0_device = methods.try_find_device()
+    if not m0_device:
         m0_device = methods.get_a_dir(is_dir=False)
+        if m0_device == 'Cancel':
+            continue
         # 如果指定了一个驱动器，说明播放器未曾导出过播放列表，因此需要创建一个播放列表存放的文件夹
-        double_chk_result = methods.double_chk(title="操作确认", content="所选驱动器不存在列表及列表文件夹，是否创建？\n若不创建，需要再次选择驱动器.")
+        double_chk_result = methods.double_chk(title="操作确认", content="所选驱动器不存在列表及列表文件夹，是否创建一个默认列表，从而添加音乐？（添加功能暂时没有）\n若不创建，需要再次选择驱动器.")
         if double_chk_result:
             mkdir(m0_device + config.Default.m0_folder)
             playlist_loop_inct = True
 
-exist_playlist = methods.chk_if_playlist(m0_device)
+    exist_playlist = methods.chk_if_playlist(m0_device)
 
-# 合并播放列表的完整路径：Default.m3u
-if exist_playlist == ("Default" or True):
-    playlist = (m0_device + config.Default.m0_folder + "\\"
-                + config.Default.playlist_default_name
-                + config.Default.m0_playlist_type)
-elif not exist_playlist:
-    playlist = (m0_device + config.Default.m0_folder + "\\"
-                + config.Default.playlist_default_name
-                + config.Default.m0_playlist_type)
-    with open(playlist, "w", encoding="utf8") as file_object:
-        file_object.write("Default")
-        print("创建了一个Default播放列表")
-else:
-    playlist = "coming_soon"  # 其他列表将在后续支持
+    # 操作第一个寻找到的文件
+    playlists = methods.get_all_files(m0_device + config.Default.m0_folder + "\\")
+    if playlists[0]:
+        playlist = (m0_device + config.Default.m0_folder + "\\"
+                    + playlists[0])
+        playlist = methods.analysis_a_playlist(playlist)
+        playlist.append('\n')
+        with open(playlist, "w", encoding="utf8") as file_object:
+            for i in playlist:
+                file_object.write(i)
 
-# 默认播放列表：读取、随机化、保存
-if playlist == "coming_soon":
-    print("暂不支持非Default的列表处理、暂不支持创建新列表")
-else:
-    with open(playlist, "r", encoding="utf8") as file_obj:
-        tmp = file_obj.readlines()
-        tmp[-1] = tmp[-1] + '\n'
-        shuffle(tmp)
-    with open(playlist, "w", encoding="utf8") as file_obj:
-        for i in tmp:
-            file_obj.write(i)
+    # 合并播放列表的完整路径：Default.m3u
+    # if exist_playlist == ("Default" or True):
+    #     playlist = (m0_device + config.Default.m0_folder + "\\"
+    #                 + config.Default.playlist_default_name
+    #                 + config.Default.m0_playlist_type)
+    # elif not exist_playlist:
+    #     playlist = (m0_device + config.Default.m0_folder + "\\"
+    #                 + config.Default.playlist_default_name
+    #                 + config.Default.m0_playlist_type)
+    #     with open(playlist, "w", encoding="utf8") as file_object:
+    #         file_object.write("Default")
+    #         print("创建了一个Default播放列表")
+    # else:
+    #     playlist = "coming_soon"  # 其他列表将在后续支持
+
+    # 默认播放列表：读取、随机化、保存
+    # if playlist == "coming_soon":
+    #     print("暂不支持非Default的列表处理、暂不支持创建新列表")
+    # else:
+    #     with open(playlist, "r", encoding="utf8") as file_obj:
+    #         tmp = file_obj.readlines()
+    #         tmp[-1] = tmp[-1] + '\n'
+    #         shuffle(tmp)
+    #     with open(playlist, "w", encoding="utf8") as file_obj:
+    #         for i in tmp:
+    #             file_obj.write(i)
--- methods.py
+++ methods.py
@@ -1,14 +1,14 @@
 # 方法库
-from os import path, listdir
+from os import path, listdir, walk
 from tkinter.filedialog import askdirectory
 from tkinter.messagebox import showinfo, askokcancel
 
 import config
 
 
-def chk_if_device():
+def try_find_device():
     """
-    以导出播放列表检查是否存在设备
+    以导出播放列表为依据，检查是否存在设备
     存在：返回 所在盘符（*:\\)
     不存在：返回 False
     """
@@ -16,11 +16,40 @@ def chk_if_device():
         driver_litter = chr(i)
         if path.isdir(driver_litter + ":\\" + config.Default.m0_folder):
             print("检查到已导出的播放列表文件夹，无需自定义路径.")
+            print(("当前设备盘符为：" + driver_litter + ":\\"))
             m0_device = driver_litter + ":\\"
             return m0_device
     else:
         print("无法检索到设备，可能是从未导出过播放列表，请自行选取设备路径.")
         return False
+
+
+def analysis_a_playlist(list_file_full_path):
+    """
+    解析一个列表，返回一个去除空行的列表
+    list_file 为传入的列表全路径，类型为 str
+    返回一个 list
+    """
+    with open(list_file_full_path, 'r', encoding='utf8') as file_object:
+        tmp_list = file_object.readlines()
+        # 去除所有的空行
+        analysis_a_playlist_loop_inct = True
+        while analysis_a_playlist_loop_inct:
+            try:
+                tmp_list.remove('\n')
+            except ValueError:
+                analysis_a_playlist_loop_inct = False
+        return tmp_list
+
+
+def get_all_files(floder_dir):
+    """
+    获取目录下所有文件
+    floder_dir 是路径（包含斜杠）
+    返回一个包含所有 文件全名 的列表
+    """
+    for root, dirs, files in walk(floder_dir):
+        return files
 
 
 def chk_if_playlist(driver_litter):
@@ -67,16 +96,21 @@ def get_a_dir(is_dir=True):
     tmp = 0
     while loop_inct:
         tmp = askdirectory()
-        if is_dir:
-            tmp = tmp.replace("/", "\\") + '\\'
-            loop_inct = False
+        if not tmp:
+            loop_inct = True
+            print('未选择任何路径，将再次选择...')
+            return 'Cancel'
         else:
-            if tmp[-1] == '/':
-                tmp = tmp.replace("/", "\\")
+            if is_dir:
+                tmp = tmp.replace("/", "\\") + '\\'
                 loop_inct = False
             else:
-                print("选择的对象不是一个驱动器的根目录，请选择驱动器的根目录（C:\\等）")
-                showinfo(title="对象选择错误", message="请选择一个驱动器的根目录，如 C:\\ ，不要选择驱动器内的文件夹.")
+                if tmp[-1] == '/':
+                    tmp = tmp.replace("/", "\\")
+                    loop_inct = False
+                else:
+                    print("选择的对象不是一个驱动器的根目录，请选择驱动器的根目录（C:\\等）")
+                    showinfo(title="对象选择错误", message="请选择一个驱动器的根目录，如 C:\\ ，不要选择驱动器内的文件夹.")
     return tmp
 
 
